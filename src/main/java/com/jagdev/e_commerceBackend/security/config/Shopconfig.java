77374a021288e59867e040ce834d917e4b035146
Jagadeesh <jagadeeshmenda2005@gmail.com>
2026-04-09 10:10:20 +0530

    spring security is added

--- src/main/java/com/jagdev/e_commerceBackend/security/config/Shopconfig.java
+++ src/main/java/com/jagdev/e_commerceBackend/security/config/Shopconfig.java
@@ -1,4 +1,4 @@
-package com.jagdev.e_commerceBackend.config;
+package com.jagdev.e_commerceBackend.security.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/jagdev/e_commerceBackend/security/user/ShopUserDetailsService.java
+++ src/main/java/com/jagdev/e_commerceBackend/security/user/ShopUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.jagdev.e_commerceBackend.security.user;
 
-public class ShopUserDetailsService {
+import com.jagdev.e_commerceBackend.model.User;
+import com.jagdev.e_commerceBackend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user= Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(
+                ()->new UsernameNotFoundException("user not found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
 }
